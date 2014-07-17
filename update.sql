540d33e5c0fbb2458ec7fee6e338030eaff70355
balack.xu <balack.xu@b5m.com>
2014-07-17 11:08:32 +0800

    fx

--- update.sql
+++ update.sql
@@ -12,13 +12,34 @@ alter table zhi_mall add tel varchar(255)  default '';
 alter table zhi_mall add addr varchar(255)  default '';
 alter table zhi_post_cate add code varchar(255)  default '';
 
-CREATE TABLE `zhi_mall_cate_re` (
-  `mall_id` int(10) NOT NULL,
-  `cate_id` smallint(4) NOT NULL,
-  UNIQUE KEY `mall_id` (`mall_id`,`cate_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+-- phpMyAdmin SQL Dump
+-- version 3.1.3
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2014 年 07 月 17 日 02:59
+-- 服务器版本: 5.1.32
+-- PHP 版本: 5.2.9
 
-CREATE TABLE `zhi_mall` (
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `zhibaby`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `zhi_shop`
+--
+
+CREATE TABLE IF NOT EXISTS `zhi_shop` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `aid` varchar(50) NOT NULL,
   `title` varchar(50) DEFAULT NULL,
@@ -45,4 +66,42 @@ CREATE TABLE `zhi_mall` (
   `tel` varchar(255) DEFAULT NULL,
   `addr` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2539 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `zhi_shop_cate`
+--
+
+CREATE TABLE IF NOT EXISTS `zhi_shop_cate` (
+  `id` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `alias` varchar(50) NOT NULL,
+  `pid` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `spid` varchar(100) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `ordid` tinyint(3) unsigned NOT NULL DEFAULT '255',
+  `seo_title` varchar(255) NOT NULL,
+  `seo_keys` varchar(255) NOT NULL,
+  `seo_desc` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `zhi_shop_comment`
+--
+
+CREATE TABLE IF NOT EXISTS `zhi_shop_comment` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `mall_id` int(10) unsigned NOT NULL,
+  `uid` int(10) unsigned NOT NULL,
+  `uname` varchar(20) NOT NULL,
+  `info` text NOT NULL,
+  `add_time` int(10) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
